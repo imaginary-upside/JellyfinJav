26e7570aed1c639f567918ee0cc40444d9a92d8b
imaginary-upside <imaginary-upside@protonmail.ch>
2021-01-30 11:40:29 -0500

    Forgot to include .dll files in release package

--- package.py
+++ package.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from hashlib import md5
 import json
 import re
+import subprocess
+import shutil
 
 tree = ET.parse("JellyfinJav/JellyfinJav.csproj")
 version = tree.find("./PropertyGroup/AssemblyVersion").text
@@ -27,8 +29,21 @@ meta = {
 Path(f"release/{version}").mkdir(parents=True, exist_ok=True)
 print(json.dumps(meta, indent=4), file=open(f"release/{version}/meta.json", "w"))
 
+subprocess.run([
+    "dotnet",
+    "build",
+    "JellyfinJav/JellyfinJav.csproj",
+    "--configuration",
+    "Release"
+])
+
+shutil.copy("JellyfinJav/bin/Release/net50/JellyfinJav.dll", f"release/{version}/")
+shutil.copy(f"{Path.home()}/.nuget/packages/anglesharp/0.14.0/lib/netstandard2.0/AngleSharp.dll", f"release/{version}/")
+
+shutil.make_archive(f"release/jellyfinjav_{version}", "zip", f"release/{version}/")
+
 entry = {
-    "checksum": md5(open(f"release/{version}/meta.json", "rb").read()).hexdigest(),
+    "checksum": md5(open(f"release/jellyfinjav_{version}.zip", "rb").read()).hexdigest(),
     "changelog": "",
     "targetAbi": f"{targetAbi}.0",
     "sourceUrl": f"https://github.com/imaginary-upside/JellyfinJav/releases/download/{version}/jellyfinjav_{version}.zip",
